7238c4c0a90bff61c982326221de6f71d95280ff
Abhinav Jain <jainabhinav2k16@gmail.com>
2021-08-13 15:50:39 +0530

    added sign up page and ability to store users in firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,22 @@ const config = {
   appId: "1:102382238807:web:5ab4aa24b19edbc2dab712",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (e) {
+      console.log("error creating user", e);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
